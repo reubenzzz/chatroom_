e4e3b3d46b6343d4cd14ab7c91e3bbd51d621893
Reuben <90466947+reubenzzz@users.noreply.github.com>
2025-09-11 07:08:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Chat-Room-Application1/',
+      base: '/Chatroom_/',
       build: {
         outDir: 'dist'
       },
